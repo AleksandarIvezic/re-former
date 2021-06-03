fd5a55bca142ee7a06d46d8613eeb7fcc58396c0
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-03 21:57:34 +0200

    Fix rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update 
+  def update
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-        redirect_to edit_user_path
-      else
-        render :edit
-      end
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
   end
 
   private
